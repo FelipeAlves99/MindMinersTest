0f9ca4172dffde8b469954f964afee49778cc328
FelipeAlves99 <falves99.dev@gmail.com>
2021-05-04 15:44:04 -0300

    Feat: working Download page

--- mind_miners_test/src/pages/DownloadPastFiles/index.js
+++ mind_miners_test/src/pages/DownloadPastFiles/index.js
@@ -1,16 +1,86 @@
-import React from "react";
+import { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+import { FiLogIn } from "react-icons/fi";
 
 import "./styles.css";
 
-export default function UploadFile() {
+const API_URL = 'https://localhost:5001';
+
+export default function DownloadFile() {
+    const [data, setData] = useState([]);
+    const [downloadLink, setDownloadLink] = useState([]);
+
+    useEffect(() => {
+
+        fetch(`${API_URL}/File`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(response => {
+                return response.json();
+            })
+            .then((data) => {
+                setData(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    const fileLink = (e) => {
+
+        console.log("entrei")
+
+        const options = {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        };
+
+        fetch(`${API_URL}/File/Download?filename=${e}`, options)
+            .then(response => response.blob())
+            .then((response) => {
+                const url = window.URL.createObjectURL(new Blob([response]));
+                setDownloadLink(url);
+            })
+            .catch(error => { console.log('request failed', error); });
+    }
+
+    const Historico = ({ links }) => {
+        return (
+            <div>
+                {!links.length ? (
+                    <h1>Não há links</h1>
+                ) : (
+                    links.map((fileName) => {
+                        return (
+                            <div key={fileName}>
+                                <p>{fileName}</p>
+                                <a href={downloadLink} download="legenda-com-offset.srt" onClick={() => fileLink(fileName) }> Download </a>
+                            </div>
+                        );
+                    })
+                )}
+            </div>
+        );
+    };
+
+
     return (
-        <div className="uploadFile-container">
-            <section className="form">
-
+        <div className="downloadFile-container">
+            <header>
                 <h1>Histórico de arquivos</h1>
-                <p>nome arquivo --- download</p>
-
-            </section>
+            </header>
+            <div className="historico">
+                <Historico links={data} />
+            </div>
+            <Link className="file-list" to="/">
+                <FiLogIn size={16} color="#e02041" />
+                    Voltar
+            </Link>
         </div>
     );
 }
